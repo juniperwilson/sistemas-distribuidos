af8f43987bd15daedb73f43f5e09eff77eb39087
juniper <juniper.figueira@gmail.com>
2025-09-17 16:38:42 +0100

    wrote list_destroy

--- SD-XX-projeto1/SD-XX-projeto1/source/list.c
+++ SD-XX-projeto1/SD-XX-projeto1/source/list.c
@@ -1,2 +1,200 @@
-
+// This file was developed by June Wilson 61795
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+#include "list-private.h"
+#include "data.h"
+
+/**
+ * @file list.h
+ * @brief Interface para a estrutura da lista de automóveis.
+ */
+
+
+/* Cria e inicializa uma nova lista de carros.
+ * Retorna a lista ou NULL em caso de erro.
+ */
+struct list_t *list_create() {
+    struct list_t* result = (struct list_t*)malloc(sizeof(struct list_t));
+    if (result == NULL) {
+        return NULL;
+    }
+    result->size = 0;
+    result->head = NULL;
+    return result;
+}
+
+/* Elimina a lista, libertando toda a memória ocupada.
+ * Retorna 0 (OK) ou -1 em caso de erro.
+ */
+int list_destroy(struct list_t *list) {
+    if (list == NULL) {
+        return -1;
+    }
+    struct car_t* current = list->head;
+    while (current->next != NULL) {
+        struct car_t* temporary = current->next;
+        free(current);
+        current = temporary;
+    }
+    return 0;
+}
+
+/* Adiciona um novo carro à lista.
+ * O carro é inserido na última posição da lista.
+ * Retorna 0 (OK) ou -1 em caso de erro.
+ */
+int list_add(struct list_t *list, struct data_t *car) {
+    if (list == NULL) {
+        return -1;
+    }
+    struct car_t* node = (struct car_t*)malloc(sizeof(struct car_t));
+    if (node == NULL) {
+        return -1;
+    }
+    node->data = car;
+    node->next = NULL;
+    
+    struct car_t* current = list->head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = node;
+    return 0;
+}
+
+/* Remove da lista o primeiro carro que corresponda ao modelo indicado.
+ * Retorna 0 se encontrou e removeu, 1 se não encontrou, ou -1 em caso de erro.
+ * DOESNT EVER RETURN -1
+ */
+int list_remove_by_model(struct list_t *list, const char *modelo) {
+    if (list == NULL) {
+        return -1;
+    }
+    struct car_t* current = list->head;
+    while (current->next != NULL) {
+        struct car_t* discard = current->next;
+        if (strcmp(discard->data->modelo, modelo) == 0) {
+            discard->next = current->next;
+            free(discard);
+            discard = NULL;
+            return 0;
+        }
+        current = current->next;
+    }
+    return 1;
+}
+
+/* Obtém o primeiro carro que corresponda à marca indicada.
+ * Retorna ponteiro para os dados ou NULL se não encontrar ou em caso de erro.
+ */
+struct data_t *list_get_by_marca(struct list_t *list, enum marca_t marca) {
+    if (list == NULL) {
+        return NULL;
+    }
+    struct car_t* current = list->head;
+    while (current != NULL) {
+        if (current->data->marca == marca) {
+            return current->data;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+/* Obtém um array de ponteiros para carros de um determinado ano.
+ * O último elemento do array é NULL.
+ * Retorna o array ou NULL em caso de erro.
+ */
+struct data_t **list_get_by_year(struct list_t *list, int ano) {
+    if (list == NULL) {
+        return NULL;
+    }
+    struct data_t** cars = (struct data_t**)malloc(sizeof(struct data_t) * list->size + 1);
+    if (cars == NULL) {
+        return NULL;
+    }
+
+    struct car_t* current = list->head;
+    int offset = 0;
+    while (current != NULL) {
+        if (current->data->ano == ano) {
+            cars[offset] = current->data;
+            offset++;
+        }
+        current = current->next;
+    }
+
+    struct data_t** result = (struct data_t**)malloc(sizeof(struct data_t) * offset + 1);
+    if (result == NULL) {
+        return NULL;
+    }
+    int i;
+    for (i = 0; i < offset; i++) {
+        result[i] = cars[i];
+    }
+    free(cars);
+    
+    return result;
+}
+
+/* Ordena a lista de carros por ano de fabrico (crescente).
+ * Retorna 0 (OK) ou -1 em caso de erro.
+ */
+int list_order_by_year(struct list_t *list) {
+    if (list == NULL) {
+        return -1;
+    }
+}
+
+/* Retorna o número de carros na lista ou -1 em caso de erro.
+ */
+int list_size(struct list_t *list) {
+    if (list == NULL) {
+        return -1;
+    }
+    int counter = 0;
+    struct car_t* pointer = list->head;
+    while (pointer != NULL) {
+        counter += 1;
+        pointer = pointer->next;
+    }
+    return counter;
+}
+
+/* Constrói um array de strings com os modelos dos carros na lista.
+ * O último elemento do array é NULL.
+ * Retorna o array ou NULL em caso de erro.
+ */
+char **list_get_model_list(struct list_t *list) {
+    if (list == NULL) {
+        return NULL;
+    }
+    char** models = (char**)malloc(sizeof(char*) * list->size);
+    if (models == NULL) {
+        return NULL;
+    }
+    struct car_t* current = list->head;
+    while (current != NULL) {
+        
+    }
+}
+
+/* Liberta a memória ocupada pelo array de modelos.
+ * Retorna 0 (OK) ou -1 em caso de erro.
+ */
+int list_free_model_list(char **models) {
+    if (models == NULL) {
+        return -1;
+    }
+    int i = 0;
+    char* current = models[0];
+    while (current != NULL) {
+        free(current);
+        current = models[++i];
+    }
+    return 0;
+}
+
+
